525a698a4699abe7607a60357542ebd88fb411e6
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2021-08-28 18:32:28 -0400

    Formulario con Hook Personalizado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook.js';
 // import { CounterApp } from './components/01-useState/CounterApp.js';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook.js';
-import { SimpleForm } from './components/02-useEffect/SimpleForm.js';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm.js';
 // import { HooksApp } from './HooksApp.js'
 
-ReactDOM.render(<SimpleForm />, document.getElementById('root'));
+ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
 
