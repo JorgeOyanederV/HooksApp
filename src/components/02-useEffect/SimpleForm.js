a4f7aa16efbeedd94ba0b44c62f94ba5f2a547bb
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2021-08-27 20:22:37 -0400

    useEffect Simple Form

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import './effects.css';
+
+export const SimpleForm = () => {
+
+   const [formState, setFormState] = useState({
+      name: '',
+      email: ''
+   })
+
+   const { name, email } = formState;
+
+   useEffect(() => {
+      console.log('hey');
+   }, [])
+
+   const handleInputChange = ({ target }) => {
+
+      setFormState({
+         ...formState,
+         [target.name]: target.value
+      })
+
+   }
+
+   return (
+      <>
+         <h1>useEffect</h1>
+         <hr />
+
+         <div className='form-group'>
+            <input
+               type='text'
+               name='name'
+               className='form-control'
+               placeholder='Tu nombre'
+               autoComplete='off'
+               value={name}
+               onChange={handleInputChange}
+            />
+         </div>
+
+         <div className='form-group'>
+            <input
+               type='email'
+               name='email'
+               className='form-control'
+               placeholder='Tu email'
+               autoComplete='off'
+               value={email}
+               onChange={handleInputChange}
+            />
+         </div>
+      </>
+   )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { CounterApp } from './components/01-useState/CounterApp.js';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook.js';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook.js';
+import { SimpleForm } from './components/02-useEffect/SimpleForm.js';
 // import { HooksApp } from './HooksApp.js'
 
-ReactDOM.render(<CounterWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<SimpleForm />, document.getElementById('root'));
 
