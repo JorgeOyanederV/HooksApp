e71d99b0f5294ffdcd3913b58a8eccc2cd7b790e
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2021-08-28 22:56:10 -0400

    useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,6 +6,8 @@ export const useFetch = (url) => {
 
    useEffect(() => {
 
+      setstate({ date: null, loading: true, error: null });
+
       fetch(url)
          .then(resp => resp.json())
          .then(data => {
