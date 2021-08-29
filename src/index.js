9d297455d5c15f7094fbac8e763459457e13e322
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2021-08-28 20:53:20 -0400

    useFetch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook.js';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook.js';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks.js';
 // import { CounterApp } from './components/01-useState/CounterApp.js';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook.js';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm.js';
 // import { HooksApp } from './HooksApp.js'
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'));
 
